12fd794535b216a11d41ca35a0da5d1e2581a37a
Maximilian Brandl <acc+git@brandlmax.com>
2021-12-02 09:53:22 +0100

    Workspace Clean

--- 01/test.py
+++
@@ -1,9 +0,0 @@
-# READ FILE
-with open('test.txt') as f:
-    input = f.readlines()
-
-# INPUT AS NUMBERS
-inputINTUngrouped = [int(numeric_string) for numeric_string in input]
-
-print(len(inputINTUngrouped))
-print(len(inputINTUngrouped) - len(inputINTUngrouped) % 3)
